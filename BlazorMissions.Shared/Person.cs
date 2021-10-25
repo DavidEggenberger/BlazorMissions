ca04fdb76a9f72fbd7206ac3c63c1d5778122259
DavidEggenberger <david99.eggenberger@gmail.com>
2021-10-25 23:22:47 +0200

    Add project files.

---
+++ BlazorMissions.Shared/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorMissions.Shared
+{
+    public class Person
+    {
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public string Name { get; set; }
+        public string Detail { get; set; }
+    }
+}
---
+++ BlazorMissions/Model/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorMissions.Model
+{
+    public class Person
+    {
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public string Name { get; set; }
+        public string Detail { get; set; }
+    }
+}
