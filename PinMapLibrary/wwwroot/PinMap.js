1d9c7980624d56a048c5948b65a594d8738a1fee
DavidEggenberger <david99.eggenberger@gmail.com>
2021-10-28 19:19:56 +0200

    Person To Agent

--- PinMapLibrary/wwwroot/PinMap.js
+++ PinMapLibrary/wwwroot/PinMap.js
@@ -23,17 +23,8 @@
             // Once it's loaded, configure it
             map.on('load', () => {
                 map.addImage('pulsing-dot', PinMap.pulsingDot(map, el), { pixelRatio: 2 });
-                map.addImage('pulsing-dot User', PinMap.pulsingDotUser(map, el), { pixelRatio: 2 });
 
                 map.addSource("pinPointSource", {
-                    "type": "geojson",
-                    "data": {
-                        "type": "FeatureCollection",
-                        "features": []
-                    }
-                });
-
-                map.addSource("UserSource", {
                     "type": "geojson",
                     "data": {
                         "type": "FeatureCollection",
@@ -47,17 +38,6 @@
                     "source": "pinPointSource",
                     "layout": {
                         "icon-image": "pulsing-dot",
-                        "text-allow-overlap": true,
-                        "icon-allow-overlap": true,
-                    }
-                });
-
-                map.addLayer({
-                    "id": "pointUser",
-                    "type": "symbol",
-                    "source": "UserSource",
-                    "layout": {
-                        "icon-image": "pulsing-dot User",
                         "text-allow-overlap": true,
                         "icon-allow-overlap": true,
                     }
@@ -149,10 +129,6 @@
             "type": "FeatureCollection",
             "features": map.pinPointFeatures
         });
-        map.getSource('UserSource').setData({
-            "type": "FeatureCollection",
-            "features": map.pinPointFeatures
-        });
     }
 
     static pulsingDot(map, el) {
@@ -206,58 +182,5 @@
             }
         };
     }
-
-    static pulsingDotUser(map, el) {
-        const size = 100;
-        return {
-            width: size,
-            height: size,
-            data: new Uint8Array(size * size * 4),
-
-            onAdd() {
-                var canvas = document.createElement('canvas');
-                canvas.width = this.width;
-                canvas.height = this.height;
-                this.context = canvas.getContext('2d');
-            },
-
-            render() {
-                var duration = 1000;
-                var t = (performance.now() % duration) / duration;
-
-                var radius = size / 2 * 0.3;
-                var outerRadius = size / 2 * 0.7 * t + radius;
-                var context = this.context;
-
-                // draw outer circle
-                context.clearRect(0, 0, this.width, this.height);
-                context.beginPath();
-                context.arc(this.width / 2, this.height / 2, outerRadius, 0, Math.PI * 2);
-                context.fillStyle = 'rgba(25, 20, 20,' + (1 - t) + ')';
-                context.fill();
-
-                // draw inner circle
-                context.beginPath();
-                context.arc(this.width / 2, this.height / 2, radius, 0, Math.PI * 2);
-                context.fillStyle = 'rgba(25, 100, 100, 1)';
-                context.strokeStyle = 'white';
-                context.lineWidth = 2 + 4 * (1 - t);
-                context.fill();
-                context.stroke();
-
-                // update this image's data with data from the canvas
-                this.data = context.getImageData(0, 0, this.width, this.height).data;
-
-                // keep the map repainting
-                if (document.body.contains(el)) {
-                    map.triggerRepaint();
-                }
-
-                // return `true` to let the map know that the image was updated
-                return true;
-            }
-        };
-    }
 }
-
 window.PinMap = PinMap;
